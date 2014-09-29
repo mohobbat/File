9b1145a4a49774ff9a080541bd616266828f42b7
mohobbat <raihan_ice07@yahoo.com>
2014-09-29 15:50:46 +0600

    added

---
+++ FileOperation/FileOperation/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace FileOperation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Name";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(230, 107);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(97, 19);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(230, 143);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(97, 19);
+            this.showButton.TabIndex = 2;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(102, 28);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(150, 20);
+            this.nameTextBox.TabIndex = 4;
+            // 
+            // nameListBox
+            // 
+            this.nameListBox.FormattingEnabled = true;
+            this.nameListBox.Location = new System.Drawing.Point(54, 204);
+            this.nameListBox.Name = "nameListBox";
+            this.nameListBox.Size = new System.Drawing.Size(207, 56);
+            this.nameListBox.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 299);
+            this.Controls.Add(this.nameListBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ReadWriteUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.ListBox nameListBox;
+    }
+}
+
---
+++ FileOperation/FileOperation/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileOperation
+{
+    public partial class Form1 : Form
+    {
+        private string filelocation = @"student.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+
+            FileStream aFileStream=new FileStream(filelocation,FileMode.Append);
+            StreamWriter aStreamWriter=new StreamWriter(aFileStream);
+            aStreamWriter.Write(nameTextBox.Text);
+            aStreamWriter.WriteLine();
+            aStreamWriter.Close();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            FileStream aFileStream=new FileStream(filelocation,FileMode.Open);
+            StreamReader aStreamReader=new StreamReader(aFileStream);
+            nameListBox.Items.Clear();
+
+            while (!aStreamReader.EndOfStream)
+            {
+                string aline = aStreamReader.ReadLine();
+                nameListBox.Items.Add(aline);
+            }
+            aStreamReader.Close();
+            
+        }
+    }
+}
